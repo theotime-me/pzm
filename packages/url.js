ae01206c08f089c7498fa6747f87167df48e7c2f
Théotime F <42675516+theotime-me@users.noreply.github.com>
2019-09-22 13:12:31 +0200

    Add files via upload

--- packages/url.js
+++ packages/url.js
@@ -1,20 +1,5 @@
-Prizm.get = param => { // param: string
-    var vars = {};
-	window.location.href.replace( location.hash, '' ).replace( 
-		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
-		function( m, key, value ) { // callback
-			vars[key] = value !== undefined ? value : '';
-		}
-	);
-
-	if ( param ) {
-		return vars[param] ? vars[param] : null;	
-	}
-	return vars;
-};
-
 Prizm.url = url => { // url: string
-	if (url){ // Si le paramètre "url" est défini
+	if (url != undefined){ // Si le paramètre "url" est défini
 		document.location.href = url; // Alors on redirige vers l'url précisée
 	}
 		// De toute façon
