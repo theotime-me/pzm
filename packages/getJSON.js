3184b59dc9bcefb57033c945e5eb8eecda305f34
Théotime F <42675516+theotime-me@users.noreply.github.com>
2019-09-22 13:06:14 +0200

    Add files via upload

---
+++ packages/getJSON.js
@@ -0,0 +1,21 @@
+Prizm.getJSON = (url, cb) => {
+    Prizm.ajax({
+        url: url,
+        async: true,
+        success(res, type) {
+            if (type.startsWith("application/json")) {
+                cb(res);
+            } else {
+                try {
+                    cb(JSON.parse(res));
+                } catch (err) {
+                    cb(res, "bad json");
+                }
+            }
+        },
+
+        error(status) {
+            cb(false, "HTTP error: "+status);
+        }
+    });
+};
