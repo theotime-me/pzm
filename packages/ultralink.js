0d090a98f947d5977ad199c64cf341b8e14ff869
Théotime F <42675516+theotime-me@users.noreply.github.com>
2020-10-03 15:49:58 +0200

    Update ultralink.js

--- packages/ultralink.js
+++ packages/ultralink.js
@@ -8,7 +8,7 @@ Prizm.ultralink = function() {
             link = new URL(el[0].href).pathname,
             icon = "M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z";
 
-        el.html(`<svg viewBox="0 0 24 24"><path d="${icon}"/></svg>
+        el.html(`<svg width="32" height="32" viewBox="0 0 24 24"><path d="${icon}"/></svg>
 <h4>${title}</h4>
 <p>${link}</p>`)
     });
